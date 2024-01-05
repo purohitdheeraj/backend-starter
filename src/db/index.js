675b139a217f771501ba2dd600c955b455ae8411
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2024-01-05 17:08:59 +0530

    express basics

--- src/db/index.js
+++ src/db/index.js
@@ -6,9 +6,7 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log("Connected to Database", connectionInstance.connection.host);
   } catch (error) {
-    console.error(`ERROR: database connection failed`, error);
     process.exit(1);
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 // require("dotenv").config();
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("DB connection Error", error);
+    });
 
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("app is listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => console.error("Error: database connection failed", error));
+
+
+
+
+
+  
 // comment
 // use IIFEs
 
